d8a34d058223bbb4a6fcd953515eed5f966325ec
Neno Neno <nenad.trencevski@gmail.com>
2020-12-07 00:02:18 +0100

    users can now create teams

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -5,6 +5,10 @@ const PlayerList = ({ allPlayers }) => {
 
     const [players, setPlayers] = useState(allPlayers);
     const [selectedFilter, setSelectedFilter] = useState(8);
+    const [selectedPlayers, setSelectedPlayers] = useState([]);
+    const [playerInput, setPlayerInput] = useState("");
+    const [teamA, setTeamA] = useState([]);
+    const [teamB, setTeamB] = useState([]);
 
     useEffect(() => {
         let clonedPlayers = [...players];
@@ -51,6 +55,65 @@ const PlayerList = ({ allPlayers }) => {
         setSelectedFilter(index);
     }
 
+    const handlePlayerChange = (e) => {
+        setPlayerInput(e.target.value);
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            addPlayer();
+        }
+    }
+
+    const handleAddPlayer = () => {
+        addPlayer();
+    }
+
+    const addPlayer = () => {
+        const foundPlayer = players.find(player => player.username.toLowerCase() === playerInput.toLowerCase());
+        if (foundPlayer) {
+            console.log(foundPlayer);
+            const checkIfDuplicate = selectedPlayers.find(aPlayer => foundPlayer.id === aPlayer.id);
+            console.log(checkIfDuplicate, "asd");
+            if (checkIfDuplicate) {
+                alert(`Player already added! ${checkIfDuplicate.username}`);
+            } else {
+                setSelectedPlayers([...selectedPlayers, foundPlayer]);
+            }
+        } else {
+            alert(`Player not found! Double-check the username`);
+        }
+        setPlayerInput("");
+    }
+
+    const handleCreateTeams = () => {
+        if (selectedPlayers.length % 2 !== 0) {
+            alert('Make sure you have an even number of players!');
+        } else {
+            const sortedPlayers = selectedPlayers.sort((a, b) => b.kdScore - a.kdScore);
+            const firstTeam = [];
+            const secondTeam = [];
+            sortedPlayers.forEach((player, index) => {
+                if (index === 0 || index === 3 || index === 5 || index === 7 || index === 9 || index === 10 || index === 12 || index === 14) {
+                    firstTeam.push(player);
+                } else {
+                    secondTeam.push(player);
+                }
+            });
+            setSelectedPlayers([]);
+            setTeamA(firstTeam);
+            setTeamB(secondTeam);
+        }
+
+    }
+
+    const handleClear = () => {
+        setTeamA([]);
+        setTeamB([]);
+        setSelectedPlayers([]);
+        setPlayerInput("");
+    }
+
     return (
         <div className="user-container">
             <div className="single-user">
@@ -70,6 +133,44 @@ const PlayerList = ({ allPlayers }) => {
                         key={player.id}
                     />
                 ))}
+            </div>
+            <div className="actions">
+                <div className="buttons-actions">
+                    <div class="input-group">
+                        <input type="text" name="text-1542372332072" id="text-1542372332072" value={playerInput} placeholder="Player Name" className="form-control" onChange={handlePlayerChange} onKeyDown={handleKeyDown} />
+                        <label for="text-1542372332072">Player Name</label>
+                    </div>
+                    <button onClick={handleAddPlayer}>Add Player</button>
+                    <button className={`create-teams ${selectedPlayers.length > 1 && "show-button"}`} onClick={handleCreateTeams}>Create Teams!</button>
+                    {teamA.length > 0 && <button onClick={handleClear}>Clear</button>}
+                </div>
+                <div>
+                    <div className="selected-players">
+                        {selectedPlayers.length > 0 && selectedPlayers.map((aPlayer, aIndex) => (
+                            <div key={aPlayer.id} className="single-selected-player">
+                                <div>{aIndex + 1}#</div>
+                                <div className="name">{aPlayer.username}</div>
+                            </div>
+                        ))}
+                    </div>
+
+                    {teamA.length > 0 && <div className="teams-container">
+                        <div>
+                            <div><strong>Team A</strong></div>
+                            {teamA.map((playerFromFirst, i) => (
+                                <div key={i}>{playerFromFirst.username}</div>
+                            ))}
+                        </div>
+                        <div className="vs"><strong>vs</strong></div>
+                        <div>
+                            <div><strong>Team B</strong></div>
+                            {teamB.map((playerFromSecond, y) => (
+                                <div key={y}>{playerFromSecond.username}</div>
+                            ))}
+                        </div>
+                    </div>
+                    }
+                </div>
             </div>
         </div>
     )
