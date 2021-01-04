5f3e8e02022f0d91967687074df06d998fc77557
Neno Neno <nenad.trencevski@gmail.com>
2021-01-04 01:18:56 +0100

    wins/losses included

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -14,12 +14,11 @@ const PlayerList = ({ allPlayers }) => {
         let clonedPlayers = [...players];
         clonedPlayers.forEach(player => {
             // 10% of the wins/losses affect the scores
-            // const winsScore = calculateWinLoseScore(player.wins, player.losses, player.draws, player.losses);
             // const lossesScore = calculateWinLoseScore(player.wins, player.losses, player.draws, player.wins);
+            const winLoseScore = calculateWinLoseScore(player.wins, player.losses);
             const kdScore = calculateKdScore(player.kills, player.deaths);
             // score based on wins/losses + k/d
-            // const totalScore = kdScore + winsScore - lossesScore;
-            const totalScore = kdScore;
+            const totalScore = kdScore + winLoseScore;
 
             player.score = totalScore.toFixed(2);
             player.killsPerGame = getKillsPerGame(player.kills, (player.wins + player.losses + player.draws));
@@ -33,6 +32,7 @@ const PlayerList = ({ allPlayers }) => {
     // const calculateKdScore = (k, d) => d !== 0 ? (k / d).toFixed(2) : "0.00";
 
     // const calculateWinLoseScore = (w, l, d, param) => param > 1 ? ((((w + l + d) / param) / 100) * 5) : 0;
+    const calculateWinLoseScore = (w, l) => ((w - l) / 100) * 2;
 
     const getKillsPerGame = (kills, numberOfGames) => Math.round((kills / numberOfGames)) || 0;
 
